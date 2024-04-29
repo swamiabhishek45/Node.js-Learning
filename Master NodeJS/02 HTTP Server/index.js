68996d84712cd32f140a4de67ce2fcf16c1386e9
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-04-30 05:13:22 +0530

    npm i express

--- Master NodeJS/02 HTTP Server/index.js
+++ Master NodeJS/02 HTTP Server/index.js
@@ -1,9 +1,10 @@
 const http = require("http");
 const fs = require("fs");
-const url = require('url');
+const url = require("url");
+const express = require("express");
 
-const myServer = http.createServer((req, res) => {
-    if(req.url === '/favicon.ico') return res.end();
+function myHandler(req, res, next) {
+    if (req.url === "/favicon.ico") return res.end();
     const log = `${Date.now()} : ${req.method} ${req.url} New Req Received\n`;
     const myUrl = url.parse(req.url, true);
     console.log(myUrl);
@@ -17,15 +18,21 @@ const myServer = http.createServer((req, res) => {
                 const userID = myUrl.query.userId;
                 res.end(`Hello, I'm ${username} and my userID is ${userID}`);
                 break;
-            case '/signup':
-                if(req.method === "GET") return res.end("Welcome to Sign Up page"); // render login form here
-                else if(req.method === "POST") return res.end("You are successfully registered!");   // after submitting the form
+            case "/signup":
+                if (req.method === "GET")
+                    return res.end(
+                        "Welcome to Sign Up page"
+                    ); // render login form here
+                else if (req.method === "POST")
+                    return res.end("You are successfully registered!"); // after submitting the form
 
             default:
-                res.end("404 not found")
+                res.end("404 not found");
         }
     });
-});
+}
+
+const myServer = http.createServer(myHandler);
 
 myServer.listen(8000, () => {
     console.log(`Server is listening on PORT 8000`);
