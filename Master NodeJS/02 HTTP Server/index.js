78687ae216d39840c97d0498c716da49ce2f0bd3
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-04-29 07:13:49 +0530

    handling URL;s

--- Master NodeJS/02 HTTP Server/index.js
+++ Master NodeJS/02 HTTP Server/index.js
@@ -1,10 +1,14 @@
 const http = require("http");
 const fs = require("fs");
+const url = require('url');
 
 const myServer = http.createServer((req, res) => {
+    if(req.url === '/favicon.ico') return res.end();
     const log = `${Date.now()} : ${req.url} New Req Received\n`;
+    const myUrl = url.parse(req.url);
+    console.log(myUrl);
     fs.appendFile("log.txt", log, (err, data) => {
-        switch (req.url) {
+        switch (myUrl.pathname) {
             case "/":
                 res.end("Welcome To Homepage");
                 break;
